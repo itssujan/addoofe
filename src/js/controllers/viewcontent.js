1c3c5a8c73d0ec35f2b4b2e18a4a38e4df2f8177
itssujan <itssujan@gmail.com>
2016-05-12 10:25:35 -0400

    view content bug

--- src/js/controllers/viewcontent.js
+++ src/js/controllers/viewcontent.js
@@ -5,16 +5,10 @@ angular.module('app')
             console.log('In ViewContent controller');
             
             // Get Content..
-            Restangular.all("video?type=video&product="+Auth.user.product+"&sort=title").getList().then(function (data) {
+            Restangular.all("video?product="+Auth.user.product+"&sort=title").getList().then(function (data) {
                 console.log("Videos count :"+data.length);
                 $scope.videos = data;
                 $scope.displayedVideos = [].concat($scope.videos);
-            });
-
-            Restangular.all("video?type=document&product="+Auth.user.product+"&sort=title").getList().then(function (data) {
-                console.log("Documents count :"+data.length);
-                $scope.documents = data;
-                $scope.displayedDocuments = [].concat($scope.documents);
             });
 
         } ]);
