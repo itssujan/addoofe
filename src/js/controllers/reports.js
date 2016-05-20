1d38f9ede4689111db5cab8a520ca855f77c9061
itssujan <itssujan@gmail.com>
2016-05-20 10:39:35 -0400

    admin reports

--- src/js/controllers/reports.js
+++ src/js/controllers/reports.js
@@ -3,4 +3,8 @@ angular.module('app')
         '$location', '$mixpanel','$filter','Auth',
         function ($scope, $state, Restangular,$rootScope,$cookieStore, growl, $location, $mixpanel,$filter,Auth) {
             console.log('In Reports controller');
+
+             Restangular.all("reports?product="+Auth.user.product).getList().then(function (data) {
+                $scope.report = data[0];
+            });
         } ]);
