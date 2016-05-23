368bf2c4c2d9fc708c697f3610c201dad9f3dea0
itssujan <itssujan@gmail.com>
2016-05-23 14:50:37 -0400

    reports

--- src/js/controllers/reports.js
+++ src/js/controllers/reports.js
@@ -6,11 +6,30 @@ angular.module('app')
 
              Restangular.all("reports?product="+Auth.user.product).getList().then(function (data) {
                 $scope.report = data[0];
+                drawTrackStatsChart();
             });
 
               Restangular.all("topvideos").getList().then(function (data) {
                 $scope.topvideos = data;
             });
+
+            // Track stats 
+            var drawTrackStatsChart = function() {
+                $scope.labels = [];
+                $scope.series = ['Total Tracks', 'Tracks Started', 'Tracks Completed'];
+                $scope.data = [[],[], []];
+                console.log("AM here");
+                angular.forEach($scope.report.trackStats,function(value,index){
+                    console.log(JSON.stringify(value));
+                    $scope.labels.push(value.title);
+                    $scope.data[0].push(value.totalTracks);
+                    $scope.data[1].push(value.tracksStarted);
+                    $scope.data[2].push(value.tracksCompleted);
+                });                
+            }
+
+
+            // End track stats  
 
               
         } ]);
