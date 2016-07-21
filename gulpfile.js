8bd472005b1c6db86bd684eb71139771ecf7938a
itssujan <itssujan@gmail.com>
2016-07-21 09:20:38 -0400

    comment out live reload

--- gulpfile.js
+++ gulpfile.js
@@ -112,7 +112,7 @@ gulp.task('watch', function () {
 gulp.task('webserver', function () {
 	connect.server({
 		root: 'dist',
-		livereload: true,
+		livereload: false,
 		port: process.env.PORT || 8888
 	});
 });
